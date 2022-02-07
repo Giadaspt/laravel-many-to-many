6b99eb14286f47a38e550e6a5b6b80bf0c6a9a85
Giadaspt <saputo.giada@gmail.com>
2022-02-07 16:17:25 +0100

    tabella ponte

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
 
 class TagsTableSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = ['Front End', 'Back End','Laravel', 'VueJs', 'Design', 'UX'];
+
+        foreach ($tags as $tag){
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($new_tag->name, '-');
+
+            $new_tag->save();
+        }
     }
 }
